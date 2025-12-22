99bb484fae68ed43eb23ad2954cabc4944b4f308
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-12-22 15:19:26 +0200

    Styled navbar and made it adaptive but without mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,203 @@ import Layout from './components/layout/Layout';
 function App() {
   return (
     <div className="app-container">
-      <Layout>Main page</Layout>
+      <Layout>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          velit ea et necessitatibus neque cupiditate nostrum cum, rerum odit
+          enim! Recusandae repudiandae vel sed aperiam hic suscipit omnis beatae
+          aspernatur, magnam ipsam, enim fugiat reiciendis minus, ea cum
+          perspiciatis nemo corrupti obcaecati laboriosam ducimus similique!
+          Ducimus earum, sint recusandae quod minima eum quia sed, obcaecati
+          omnis alias incidunt quos hic pariatur qui repellat et laboriosam odit
+          modi suscipit eligendi expedita? Nulla deleniti provident, iure est
+          nemo tenetur neque doloremque aliquam ipsam eaque magni eligendi quae
+          tempore accusamus recusandae culpa blanditiis vero soluta. Ab, beatae
+          explicabo dolor praesentium rerum reiciendis. Molestias!{' '}
+        </p>
+      </Layout>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,23 @@
-// import styles from './Navbar.module.css';
+import styles from './Navbar.module.css';
+import arrow_icon from '../../assets/Arrow.svg';
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>Area</li>
-          <li>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navLogo}>Area</li>
+          <li className={styles.navItems}>
             <a href="#">Benefits</a>
             <a href="#">Specifications</a>
             <a href="#">How-to</a>
             <a href="#">Contact Us</a>
+          </li>
+          <li className={styles.learnMoreBtn}>
+            <a href="#" className={styles.learnMoreLink}>
+              Learn More
+            </a>
+            <img src={arrow_icon} alt="arrow icon for learn more button" />
           </li>
         </ul>
       </nav>
