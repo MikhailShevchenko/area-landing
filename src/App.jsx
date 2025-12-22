9576e4fb878d2d6275eafdfb2899cfac276b7ce3
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-12-22 13:50:18 +0200

    Base structure of the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Layout from './components/layout/Layout';
 
 function App() {
-  return <></>;
+  return (
+    <div className="app-container">
+      <Layout>Main page</Layout>
+    </div>
+  );
 }
 
 export default App;
