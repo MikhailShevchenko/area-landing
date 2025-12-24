fed429d61c849b87ae581e63cebdb8d5891dabd0
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-12-24 15:28:14 +0200

    Finished responsive footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,37 @@
 import styles from './Footer.module.css';
+import union_img from '../../assets/Union.svg';
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <hr className={styles.footerLine} />
+      <div>
+        <ul className={styles.footerMenu}>
+          <li className={styles.footerMenuItem}>
+            <a href="#">Benefits</a>
+          </li>
+          <li className={styles.footerMenuItem}>
+            <a href="#">Specifications</a>
+          </li>
+          <li className={styles.footerMenuItem}>
+            <a href="#">How-to</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div className={styles.copyRight}>
+          <div>
+            <img src={union_img} alt="" />
+            <span>
+              <span>© Area.</span>
+              <span>2025</span>
+            </span>
+          </div>
+          <span>All Rights Reserved</span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
