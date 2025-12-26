889f05c251f1a1ddebab373d8b97c1a2d11d922d
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-12-26 22:09:05 +0200

    Changed app structure - moved header section to main

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="app-container">
       <Layout>
-        <Header />
         <main>
+          <Header />
           <LogoCloud />
           <Benefits />
           <Features />
