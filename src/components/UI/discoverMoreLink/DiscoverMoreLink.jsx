ce0760bce868bb80ce1a261dd1257777d18d678d
MikhailShevchenko <rogerthatoff@gmail.com>
2026-01-09 10:45:25 +0200

    Deploy - corrected discover more link html structure and styles

--- src/components/UI/discoverMoreLink/DiscoverMoreLink.jsx
+++ src/components/UI/discoverMoreLink/DiscoverMoreLink.jsx
@@ -1,27 +1,15 @@
-import { useRef } from 'react';
 import styles from './DiscoverMoreLink.module.css';
 
 const DiscoverMoreLink = () => {
-  const discoverLinkRef = useRef(null);
-
-  const handleLink = () => {
-    if (discoverLinkRef.current) {
-      discoverLinkRef.current.click();
-    }
-  };
-
   return (
     <>
-      <div className={styles.discoverMoreLink} onClick={handleLink}>
-        <a
-          ref={discoverLinkRef}
-          href="https://github.com/MykhailoShevchenko12/area-landing"
-          target="_blank"
-          onClick={(e) => e.stopPropagation()}
-        >
-          Discover More
-        </a>
-      </div>
+      <a
+        href="https://github.com/MykhailoShevchenko12/area-landing"
+        target="_blank"
+        className={styles.discoverMoreLink}
+      >
+        Discover More
+      </a>
     </>
   );
 };
