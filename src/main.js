5f211e2e111df4fbdde98e43b88823dd30535e01
MikhailShevchenko <rogerthatoff@gmail.com>
2026-01-28 22:43:59 +0200

    Navbar with responsive behavior and animation effects, also added new font and all the necessary icons

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import './main.css';
+import './styles/navbar.css';
+
+const burgerButton = document.getElementById('burger');
+const mobileMenuOpen = document.getElementById('burger-open');
+const mobileMenuClose = document.getElementById('burger-close');
+const mobileMenu = document.getElementById('mobile-menu');
+const links = document.querySelectorAll('.link--hovered');
+
+function mobileMenuHandler() {
+  if (mobileMenuOpen && mobileMenuClose && mobileMenu) {
+    mobileMenuOpen.classList.toggle('navbar-burger__open--hidden');
+    mobileMenuClose.classList.toggle('navbar-burger__close--visible');
+    const isOpen = mobileMenu.classList.toggle('mobile-menu--visible');
+
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+}
+
+if (burgerButton) {
+  burgerButton.addEventListener('click', mobileMenuHandler);
+}
+
+if (links) {
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      if (mobileMenu.classList.contains('mobile-menu--visible')) {
+        mobileMenuHandler();
+      }
+    });
+  });
+}
